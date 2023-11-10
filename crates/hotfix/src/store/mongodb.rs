e0e8ef7aacb6a693b5556526a9908ce76d3e8ad9
David Steiner <david_j_steiner@yahoo.co.nz>
2023-11-10 16:01:27 +0000

    Initialise MongoDB store correctly

--- crates/hotfix/src/store/mongodb.rs
+++ crates/hotfix/src/store/mongodb.rs
@@ -1,29 +1,66 @@
 use async_trait::async_trait;
-use mongodb::Client;
+use mongodb::bson::doc;
+use mongodb::bson::oid::ObjectId;
+use mongodb::bson::Binary;
+use mongodb::options::FindOneOptions;
+use mongodb::{Collection, Database};
+use serde::{Deserialize, Serialize};
 
 use crate::store::MessageStore;
 
+#[derive(Debug, Deserialize, Serialize)]
+struct SequenceMeta {
+    #[serde(rename = "_id")]
+    object_id: ObjectId,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Message {
+    sequence_id: u64,
+    data: Binary,
+}
+
 #[allow(dead_code)]
 struct MongoDbMessageStore {
-    client: Client,
-    database_name: String,
-    collection_name: String,
+    meta_collection: Collection<SequenceMeta>,
+    message_collection: Collection<Message>,
+    sequence_id: ObjectId,
 }
 
 impl MongoDbMessageStore {
     #[allow(dead_code)]
-    pub async fn new(
-        client: Client,
-        database_name: Option<&str>,
-        collection_name: Option<&str>,
-    ) -> Self {
-        let database_name = database_name.unwrap_or("hotfix");
+    pub async fn new(db: Database, collection_name: Option<&str>) -> Self {
         let collection_name = collection_name.unwrap_or("messages");
+        let meta_collection = db.collection(collection_name);
+        let message_collection = db.collection(collection_name);
+
+        let sequence_id = Self::get_or_default_sequence(&meta_collection).await;
 
         Self {
-            client,
-            database_name: database_name.to_string(),
-            collection_name: collection_name.to_string(),
+            meta_collection,
+            message_collection,
+            sequence_id,
+        }
+    }
+
+    async fn get_or_default_sequence(meta_collection: &Collection<SequenceMeta>) -> ObjectId {
+        let options = FindOneOptions::builder().sort(doc! { "_id": -1 }).build();
+        let meta = meta_collection.find_one(doc! {}, options).await.unwrap();
+
+        match meta {
+            None => {
+                let sequence_id = ObjectId::new();
+                let initial_meta = SequenceMeta {
+                    object_id: sequence_id,
+                };
+                meta_collection
+                    .insert_one(initial_meta, None)
+                    .await
+                    .unwrap();
+
+                sequence_id
+            }
+            Some(meta) => meta.object_id,
         }
     }
 }
