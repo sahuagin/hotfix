2f2f6d852c49ba0f6ed9c6953bce14937952b219
David Steiner <david_j_steiner@yahoo.co.nz>
2025-06-27 16:50:30 -0400

    Add tests for weekdays schedules

--- crates/hotfix/src/session_schedule.rs
+++ crates/hotfix/src/session_schedule.rs
@@ -44,8 +44,11 @@ impl SessionSchedule {
                 weekdays,
                 start_time,
                 end_time,
-                timezone: _,
-            } => Self::check_weekdays_schedule(datetime, weekdays, start_time, end_time),
+                timezone,
+            } => {
+                let adjusted_datetime = datetime.with_timezone(timezone);
+                Self::check_weekdays_schedule(&adjusted_datetime, weekdays, start_time, end_time)
+            }
             SessionSchedule::Weekly { .. } => false,
         }
     }
@@ -63,7 +66,7 @@ impl SessionSchedule {
     }
 
     fn check_weekdays_schedule(
-        datetime: &DateTime<Utc>,
+        datetime: &DateTime<Tz>,
         weekdays: &[Weekday],
         start_time: &NaiveTime,
         end_time: &NaiveTime,
@@ -395,6 +398,390 @@ mod tests {
             Utc,
         );
         assert!(!schedule_1.is_active_at(&at_end));
+    }
+
+    #[test]
+    fn test_active_at_weekdays_schedule_utc() {
+        // Monday to Friday, 9AM to 5PM UTC
+        let schedule = SessionSchedule::Weekdays {
+            start_time: NaiveTime::from_hms_opt(9, 0, 0).unwrap(),
+            end_time: NaiveTime::from_hms_opt(17, 0, 0).unwrap(),
+            weekdays: vec![
+                Weekday::Mon,
+                Weekday::Tue,
+                Weekday::Wed,
+                Weekday::Thu,
+                Weekday::Fri,
+            ],
+            timezone: Tz::UTC,
+        };
+
+        // Monday 8:59:59 - before start time
+        let monday_before_start = DateTime::from_naive_utc_and_offset(
+            chrono::NaiveDate::from_ymd_opt(2025, 6, 30) // Monday
+                .unwrap()
+                .and_hms_opt(8, 59, 59)
+                .unwrap(),
+            Utc,
+        );
+        assert!(!schedule.is_active_at(&monday_before_start));
+
+        // Monday 9:00:01 - after start time
+        let monday_after_start = DateTime::from_naive_utc_and_offset(
+            chrono::NaiveDate::from_ymd_opt(2025, 6, 30) // Monday
+                .unwrap()
+                .and_hms_opt(9, 0, 1)
+                .unwrap(),
+            Utc,
+        );
+        assert!(schedule.is_active_at(&monday_after_start));
+
+        // Friday 16:59:59 - just before end time on a valid day
+        let friday_before_end = DateTime::from_naive_utc_and_offset(
+            chrono::NaiveDate::from_ymd_opt(2025, 7, 4) // Friday
+                .unwrap()
+                .and_hms_opt(16, 59, 59)
+                .unwrap(),
+            Utc,
+        );
+        assert!(schedule.is_active_at(&friday_before_end));
+
+        // Friday 17:00:00 - at end time on a valid day (exclusive end)
+        let friday_at_end = DateTime::from_naive_utc_and_offset(
+            chrono::NaiveDate::from_ymd_opt(2025, 7, 4) // Friday
+                .unwrap()
+                .and_hms_opt(17, 0, 0)
+                .unwrap(),
+            Utc,
+        );
+        assert!(!schedule.is_active_at(&friday_at_end));
+
+        // Saturday 12:00:00 - middle of day on weekend
+        let saturday = DateTime::from_naive_utc_and_offset(
+            chrono::NaiveDate::from_ymd_opt(2025, 7, 5) // Saturday
+                .unwrap()
+                .and_hms_opt(12, 0, 0)
+                .unwrap(),
+            Utc,
+        );
+        assert!(!schedule.is_active_at(&saturday));
+    }
+
+    #[test]
+    fn test_active_at_weekdays_schedule_london() {
+        // Monday to Friday, 9AM to 5PM London time
+        // During summer (June), London is UTC+1
+        let schedule = SessionSchedule::Weekdays {
+            start_time: NaiveTime::from_hms_opt(9, 0, 0).unwrap(),
+            end_time: NaiveTime::from_hms_opt(17, 0, 0).unwrap(),
+            weekdays: vec![
+                Weekday::Mon,
+                Weekday::Tue,
+                Weekday::Wed,
+                Weekday::Thu,
+                Weekday::Fri,
+            ],
+            timezone: Tz::Europe__London,
+        };
+
+        // Monday 7:59:59 UTC = 8:59:59 London - before start time
+        let monday_before_start = DateTime::from_naive_utc_and_offset(
+            chrono::NaiveDate::from_ymd_opt(2025, 6, 30) // Monday
+                .unwrap()
+                .and_hms_opt(7, 59, 59)
+                .unwrap(),
+            Utc,
+        );
+        assert!(!schedule.is_active_at(&monday_before_start));
+
+        // Monday 8:00:01 UTC = 9:00:01 London - after start time
+        let monday_after_start = DateTime::from_naive_utc_and_offset(
+            chrono::NaiveDate::from_ymd_opt(2025, 6, 30) // Monday
+                .unwrap()
+                .and_hms_opt(8, 0, 1)
+                .unwrap(),
+            Utc,
+        );
+        assert!(schedule.is_active_at(&monday_after_start));
+
+        // Friday 15:59:59 UTC = 16:59:59 London - just before end time
+        let friday_before_end = DateTime::from_naive_utc_and_offset(
+            chrono::NaiveDate::from_ymd_opt(2025, 7, 4) // Friday
+                .unwrap()
+                .and_hms_opt(15, 59, 59)
+                .unwrap(),
+            Utc,
+        );
+        assert!(schedule.is_active_at(&friday_before_end));
+
+        // Friday 16:00:00 UTC = 17:00:00 London - at end time (exclusive)
+        let friday_at_end = DateTime::from_naive_utc_and_offset(
+            chrono::NaiveDate::from_ymd_opt(2025, 7, 4) // Friday
+                .unwrap()
+                .and_hms_opt(16, 0, 0)
+                .unwrap(),
+            Utc,
+        );
+        assert!(!schedule.is_active_at(&friday_at_end));
+    }
+
+    #[test]
+    fn test_active_at_weekdays_schedule_newyork() {
+        // Monday to Friday, 9:30AM to 4PM New York time
+        // During summer (June), New York is UTC-4
+        let schedule = SessionSchedule::Weekdays {
+            start_time: NaiveTime::from_hms_opt(9, 30, 0).unwrap(),
+            end_time: NaiveTime::from_hms_opt(16, 0, 0).unwrap(),
+            weekdays: vec![
+                Weekday::Mon,
+                Weekday::Tue,
+                Weekday::Wed,
+                Weekday::Thu,
+                Weekday::Fri,
+            ],
+            timezone: Tz::America__New_York,
+        };
+
+        // Monday 13:29:59 UTC = 9:29:59 NY - just before start time
+        let monday_before_start = DateTime::from_naive_utc_and_offset(
+            chrono::NaiveDate::from_ymd_opt(2025, 6, 30) // Monday
+                .unwrap()
+                .and_hms_opt(13, 29, 59)
+                .unwrap(),
+            Utc,
+        );
+        assert!(!schedule.is_active_at(&monday_before_start));
+
+        // Monday 13:30:01 UTC = 9:30:01 NY - just after start time
+        let monday_after_start = DateTime::from_naive_utc_and_offset(
+            chrono::NaiveDate::from_ymd_opt(2025, 6, 30) // Monday
+                .unwrap()
+                .and_hms_opt(13, 30, 1)
+                .unwrap(),
+            Utc,
+        );
+        assert!(schedule.is_active_at(&monday_after_start));
+
+        // Tuesday 19:59:59 UTC = 15:59:59 NY - just before end time
+        let tuesday_before_end = DateTime::from_naive_utc_and_offset(
+            chrono::NaiveDate::from_ymd_opt(2025, 7, 1) // Tuesday
+                .unwrap()
+                .and_hms_opt(19, 59, 59)
+                .unwrap(),
+            Utc,
+        );
+        assert!(schedule.is_active_at(&tuesday_before_end));
+
+        // Tuesday 20:00:00 UTC = 16:00:00 NY - at end time (exclusive)
+        let tuesday_at_end = DateTime::from_naive_utc_and_offset(
+            chrono::NaiveDate::from_ymd_opt(2025, 7, 1) // Tuesday
+                .unwrap()
+                .and_hms_opt(20, 0, 0)
+                .unwrap(),
+            Utc,
+        );
+        assert!(!schedule.is_active_at(&tuesday_at_end));
+    }
+
+    #[test]
+    fn test_active_at_weekdays_schedule_sydney_crossing_midnight() {
+        // Monday to Friday, 10PM to 6AM Sydney time (crosses midnight)
+        // In June, Sydney is UTC+10
+        let schedule = SessionSchedule::Weekdays {
+            start_time: NaiveTime::from_hms_opt(22, 0, 0).unwrap(),
+            end_time: NaiveTime::from_hms_opt(6, 0, 0).unwrap(),
+            weekdays: vec![
+                Weekday::Mon,
+                Weekday::Tue,
+                Weekday::Wed,
+                Weekday::Thu,
+                Weekday::Fri,
+            ],
+            timezone: Tz::Australia__Sydney,
+        };
+
+        // Monday 11:59:59 UTC = 21:59:59 Sydney - just before start time
+        let monday_before_start = DateTime::from_naive_utc_and_offset(
+            chrono::NaiveDate::from_ymd_opt(2025, 6, 30) // Monday
+                .unwrap()
+                .and_hms_opt(11, 59, 59)
+                .unwrap(),
+            Utc,
+        );
+        assert!(!schedule.is_active_at(&monday_before_start));
+
+        // Monday 12:00:01 UTC = 22:00:01 Sydney - just after start time
+        let monday_after_start = DateTime::from_naive_utc_and_offset(
+            chrono::NaiveDate::from_ymd_opt(2025, 6, 30) // Monday
+                .unwrap()
+                .and_hms_opt(12, 0, 1)
+                .unwrap(),
+            Utc,
+        );
+        assert!(schedule.is_active_at(&monday_after_start));
+
+        // Tuesday 19:59:59 UTC = 5:59:59 Sydney Wednesday - just before end time
+        let tuesday_before_end = DateTime::from_naive_utc_and_offset(
+            chrono::NaiveDate::from_ymd_opt(2025, 7, 1) // Tuesday
+                .unwrap()
+                .and_hms_opt(19, 59, 59)
+                .unwrap(),
+            Utc,
+        );
+        assert!(schedule.is_active_at(&tuesday_before_end));
+
+        // Tuesday 20:00:00 UTC = 6:00:00 Sydney Wednesday - at end time (exclusive)
+        let tuesday_at_end = DateTime::from_naive_utc_and_offset(
+            chrono::NaiveDate::from_ymd_opt(2025, 7, 1) // Tuesday
+                .unwrap()
+                .and_hms_opt(20, 0, 0)
+                .unwrap(),
+            Utc,
+        );
+        assert!(!schedule.is_active_at(&tuesday_at_end));
+
+        // Wednesday 10:00:00 UTC = 20:00:00 Sydney - during inactive period
+        let wednesday_inactive = DateTime::from_naive_utc_and_offset(
+            chrono::NaiveDate::from_ymd_opt(2025, 7, 2) // Wednesday
+                .unwrap()
+                .and_hms_opt(10, 0, 0)
+                .unwrap(),
+            Utc,
+        );
+        assert!(!schedule.is_active_at(&wednesday_inactive));
+    }
+
+    #[test]
+    fn test_active_at_weekdays_schedule_only_weekend() {
+        // Weekend schedule (Saturday and Sunday), 10AM to 4PM UTC
+        let schedule = SessionSchedule::Weekdays {
+            start_time: NaiveTime::from_hms_opt(10, 0, 0).unwrap(),
+            end_time: NaiveTime::from_hms_opt(16, 0, 0).unwrap(),
+            weekdays: vec![Weekday::Sat, Weekday::Sun],
+            timezone: Tz::UTC,
+        };
+
+        // Friday 12:00:00 - should be inactive (weekday)
+        let friday = DateTime::from_naive_utc_and_offset(
+            chrono::NaiveDate::from_ymd_opt(2025, 7, 4) // Friday
+                .unwrap()
+                .and_hms_opt(12, 0, 0)
+                .unwrap(),
+            Utc,
+        );
+        assert!(!schedule.is_active_at(&friday));
+
+        // Saturday 9:59:59 - before start time on weekend
+        let saturday_before_start = DateTime::from_naive_utc_and_offset(
+            chrono::NaiveDate::from_ymd_opt(2025, 7, 5) // Saturday
+                .unwrap()
+                .and_hms_opt(9, 59, 59)
+                .unwrap(),
+            Utc,
+        );
+        assert!(!schedule.is_active_at(&saturday_before_start));
+
+        // Saturday 10:00:01 - after start time on weekend
+        let saturday_after_start = DateTime::from_naive_utc_and_offset(
+            chrono::NaiveDate::from_ymd_opt(2025, 7, 5) // Saturday
+                .unwrap()
+                .and_hms_opt(10, 0, 1)
+                .unwrap(),
+            Utc,
+        );
+        assert!(schedule.is_active_at(&saturday_after_start));
+
+        // Sunday 15:59:59 - before end time on weekend
+        let sunday_before_end = DateTime::from_naive_utc_and_offset(
+            chrono::NaiveDate::from_ymd_opt(2025, 7, 6) // Sunday
+                .unwrap()
+                .and_hms_opt(15, 59, 59)
+                .unwrap(),
+            Utc,
+        );
+        assert!(schedule.is_active_at(&sunday_before_end));
+    }
+
+    #[test]
+    fn test_active_at_weekdays_schedule_overnight_crossing_weekdays() {
+        // Monday to Thursday nights, 10PM to 6AM London time
+        // During summer (June), London is UTC+1
+        let schedule = SessionSchedule::Weekdays {
+            start_time: NaiveTime::from_hms_opt(22, 0, 0).unwrap(),
+            end_time: NaiveTime::from_hms_opt(6, 0, 0).unwrap(),
+            weekdays: vec![Weekday::Mon, Weekday::Tue, Weekday::Wed, Weekday::Thu],
+            timezone: Tz::Europe__London,
+        };
+
+        // Monday 20:59:59 UTC = 21:59:59 London - just before start time
+        let monday_before_start = DateTime::from_naive_utc_and_offset(
+            chrono::NaiveDate::from_ymd_opt(2025, 6, 30) // Monday
+                .unwrap()
+                .and_hms_opt(20, 59, 59)
+                .unwrap(),
+            Utc,
+        );
+        assert!(!schedule.is_active_at(&monday_before_start));
+
+        // Monday 21:00:01 UTC = 22:00:01 London - just after start time
+        let monday_after_start = DateTime::from_naive_utc_and_offset(
+            chrono::NaiveDate::from_ymd_opt(2025, 6, 30) // Monday
+                .unwrap()
+                .and_hms_opt(21, 0, 1)
+                .unwrap(),
+            Utc,
+        );
+        assert!(schedule.is_active_at(&monday_after_start));
+
+        // Tuesday 4:59:59 UTC = 5:59:59 London - just before end time, still Monday's session
+        let tuesday_before_end = DateTime::from_naive_utc_and_offset(
+            chrono::NaiveDate::from_ymd_opt(2025, 7, 1) // Tuesday
+                .unwrap()
+                .and_hms_opt(4, 59, 59)
+                .unwrap(),
+            Utc,
+        );
+        assert!(schedule.is_active_at(&tuesday_before_end));
+
+        // Tuesday 5:00:00 UTC = 6:00:00 London - at end time, should be inactive
+        let tuesday_at_end = DateTime::from_naive_utc_and_offset(
+            chrono::NaiveDate::from_ymd_opt(2025, 7, 1) // Tuesday
+                .unwrap()
+                .and_hms_opt(5, 0, 0)
+                .unwrap(),
+            Utc,
+        );
+        assert!(!schedule.is_active_at(&tuesday_at_end));
+
+        // Friday 21:00:01 UTC = 22:00:01 London - after start time but on Friday which is excluded
+        let friday_night = DateTime::from_naive_utc_and_offset(
+            chrono::NaiveDate::from_ymd_opt(2025, 7, 4) // Friday
+                .unwrap()
+                .and_hms_opt(21, 0, 1)
+                .unwrap(),
+            Utc,
+        );
+        assert!(!schedule.is_active_at(&friday_night));
+
+        // Thursday 21:00:01 UTC = 22:00:01 London - Thursday night session should be active
+        let thursday_night = DateTime::from_naive_utc_and_offset(
+            chrono::NaiveDate::from_ymd_opt(2025, 7, 3) // Thursday
+                .unwrap()
+                .and_hms_opt(21, 0, 1)
+                .unwrap(),
+            Utc,
+        );
+        assert!(schedule.is_active_at(&thursday_night));
+
+        // Friday 4:59:59 UTC = 5:59:59 London - still Thursday's session ending on Friday morning
+        let friday_morning = DateTime::from_naive_utc_and_offset(
+            chrono::NaiveDate::from_ymd_opt(2025, 7, 4) // Friday
+                .unwrap()
+                .and_hms_opt(4, 59, 59)
+                .unwrap(),
+            Utc,
+        );
+        assert!(schedule.is_active_at(&friday_morning));
     }
 
     #[test]
